7f2fc4f18d81f12073d1a59770b010e6da0119e1
Indra Iswara <indraiswara@Indras-MacBook-Pro.local>
2026-03-04 14:35:06 +0800

    refactor: restructure repo - move source code into app/ subdirectory, keep docker-compose.yml and Caddyfile at root

--- find_was_panganten.js
+++
@@ -1,3 +0,0 @@
-import { getBaliDate } from './src/utils/bali-calendar.ts';
-
-// We need to run this with ts-node or something. Let's just create a simpler script or use tsx
--- test-pawiwahan.js
+++
@@ -1,4 +0,0 @@
-const { getBaliDate } = require('./src/utils/bali-calendar');
-const { calculatePawiwahanScore } = require('./src/utils/pawiwahan-score');
-
-// We can't easily require ts files without ts-node, so let's just inspect the logic.
